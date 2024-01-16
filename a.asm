929b0673cb56b7455f377ef3bc9c9e303dd92f48
Sassi <155637760+Sassi1000@users.noreply.github.com>
2024-01-16 21:11:59 +0200

    Add files via upload

--- a.asm
+++ a.asm
@@ -1,46 +1,28 @@
-cseg   segment
-assume cs:cseg
+dseg segment
 
+d db 54h, 0b7h, 9fh, 46h, 0deh
 
-start:  mov cx, 2c5fh
-        mov dl, ch
-        shr dl, 4
-        cmp dl, 10
-        jnc ItIsL
-        add dl, 30h
-        jmp Show
-ItIsL:  add dl, 87
- Show:  mov ah, 2
-        int 21h
-        and ch, 0fh
-        mov dl, ch
-        cmp dl, 10
-        jnc ItIsLt
-        add dl, 30h
-        jmp Show2
-ItIsLt: add dl, 87
- Show2: int 21h
+dseg ends
 
-        mov dl, cl   ;5f
-        shr dl, 4    ;5
-        cmp dl, 10
-        jnc text
-        add dl, 30h
-        jmp show3
-  text: add dl, 87
- show3: int 21h
-        and cl, 0fh
-        mov dl, cl   ;f
-        cmp dl, 10
-        jnc text2
-        add dl, 30h
-        jmp show4
- text2: add dl, 87
- show4: int 21h
+cseg segment
+assume cs:cseg, ds:dseg
 
-        int 3h
+     Start: mov ax, dseg
+            mov ds, ax
+            mov ah, 2
+            mov si, 0
+     Again: mov dh, d[si]
+            mov ch, 8
+    Again2: rcr dh, 1
+            rcl dl, 1
+            dec ch
+            jnz Again2
+            inc si
+            cmp si, 5
+            jc Again
 
-          
-cseg ends 
+      End: int 3h
+
+cseg ends
 end Start
 
